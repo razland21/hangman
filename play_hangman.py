7c6537149b5a575e65e845a8ca2f10146103d1ee
Rica <razarcon@gmail.com>
2017-05-02 13:49:41 -0700

    select_answer() and valid_guess() working

--- play_hangman.py
+++ play_hangman.py
@@ -4,8 +4,8 @@ word_list = ["cat fur", "sunshine", "silly bear", "fantastic", "this is a long p
 
 
 #[fn] select one word randomly from word_list (answer)
-def select_answer()
-	select_word(word_list).upper()
+def select_answer(lst):
+	return lst[randint(0,len(lst)-1)]
 
 
 #[fn] create game board
@@ -13,34 +13,61 @@ def select_answer()
 		# - initial board will contain _ for each letter in answer
 		# - if spaces appear in answer, the board will contain a space instead of _  
 
-def create_board(word)
+def create_board(word):
+	pass
 
 # [fn] print game board
 	# - print board to show up as one line, not appearing as the list. 
 		# ex: the cat shows up as _ _ _   _ _ _
 	# let player know how many more guesses they have (later: use number of guesses to show game art)
 
-def print_board(board, guesses_left)
-
+def print_board(board, guesses_left):
+	pass
 
 # [fn] update board to show where letter appears
 	#- find indices where guess is located and replace _ with letter.
 	
-def update_board(guess, board)
-
+def update_board(guess, board):
+	pass
 
 # [fn] check if board shows complete answer
-def player_wins(game_board)
+def player_wins(game_board):
+	pass
 			
+	
+# [fn] check validity of input, and reprompt player if any of these conditions happen:
+	# no characters 
+	# more than one character
+	# has non-alpha characters
+	# guess was already made			
+
+def valid_guess(guess, guess_list):
+	if len(guess) == 0:
+		print "You need to guess something!  \n"
+		return False
+	elif not guess.isalpha():
+		print "You need to enter an actual letter. \n"
+		return False
+	elif len(guess) > 1:
+		print "You can only guess one letter at a time.  \n"
+		return False
+	elif guess in guess_list:
+		print "You have already guessed {}. Try another letter. \n".format(guess)
+		return False
+	else:
+		return True
+	
+
+
 #[fn] main function to start hangman game
-def play_hangman()
+def play_hangman():
 	guess_list = []
 	game_board = []
 	num_guesses = 0
 	guesses_left = 6
 
 	#[fn] select one word randomly from word_list (answer)
-	answer = select_answer()
+	answer = select_answer(word_list)
 	
 	#[fn] create game board
 	game_board = create_board(answer)
@@ -53,14 +80,12 @@ def play_hangman()
 		print_board(game_board, guesses_left)
 			
 		# prompt player to guess a letter (guess)
-		
 		guess = raw_input("Guess one letter: ").strip().upper()
 		
-		# check validity of input, and reprompt player if any of these conditions happen:
-			# no characters 
-			# more than one character
-			# has non-alpha characters
-			# guess was already made
+		# check validity of input
+		if not valid_guess(guess, guess_list):
+			continue
+		
 				
 		# [fn] if guess is in answer:
 		if guess in answer:
@@ -70,7 +95,8 @@ def play_hangman()
 			# [fn*] print updated board 
 			print_board(game_board, guesses_left)
 			# [fn] check if board shows complete answer
-			if player_wins(game_board)
+			if player_wins(game_board):
+				pass
 				# if yes, player wins
 				# if no, continue playing
 			
